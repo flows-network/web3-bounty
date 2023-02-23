295e275d772944fc339bf0a6622ba71232f3af79
DarumaDocker <DarumaDocker@gmail.com>
2023-02-23 16:32:21 +0800

    flow for email verify

--- flows/email-send/src/lib.rs
+++ flows/email-send/src/lib.rs
@@ -1,27 +1,52 @@
 use lambda_flows::{request_received, send_response};
+use rand::{distributions::Alphanumeric, Rng};
 use sendgrid_flows::{send_email, Email};
-use store_flows::global_get;
+use store_flows::global_set;
 
 #[no_mangle]
 pub fn run() {
     request_received(|qry, _body| {
-        if let Some(to) = qry.get("to") {
-            if let Some(to) = to.as_str() {
-                let email = Email {
-                    to: vec![String::from(to)],
-                    subject: String::from("Hi"),
-                    content: String::from("New Bee"),
-                };
-                send_email("wangshishuo@wespoke.com", &email);
+        if let Some(account) = qry.get("account") {
+            if let Some(account) = account.as_str() {
+                if let Some(to) = qry.get("to") {
+                    if let Some(to) = to.as_str() {
+                        let challenge = random_challenge();
+                        global_set(
+                            "web3bounty_email_challenge",
+                            serde_json::json!({
+                                "account": account,
+                                "email": to,
+                                "challenge": challenge
+                            }),
+                        );
+
+                        let verifier_lambda = std::env::var("VERIFIER_LAMBDA").unwrap();
+
+                        let email = Email {
+                            to: vec![String::from(to)],
+                            subject: String::from("Verify your email"),
+                            content: String::from(format!(
+                                r#"
+Click the link below to verify your email<br/>
+<a href="{verifier_lambda}?challenge={challenge}">{verifier_lambda}?challenge={challenge}</a>"#
+                            )),
+                        };
+                        match send_email("wangshishuo@wespoke.com", &email) {
+                            Ok(_) => return send_response(200, vec![], vec![]),
+                            Err(e) => return send_response(500, vec![], e.as_bytes().to_vec()),
+                        }
+                    }
+                }
             }
         }
-        send_response(
-            302,
-            vec![(
-                String::from("location"),
-                std::env::var("REDIRECT_URI").unwrap(),
-            )],
-            vec![],
-        );
+        send_response(400, vec![], vec![]);
     });
+}
+
+fn random_challenge() -> String {
+    rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(10)
+        .map(char::from)
+        .collect()
 }
---
+++ flows/email-verify/src/lib.rs
@@ -0,0 +1,50 @@
+use airtable_flows::create_record;
+use lambda_flows::{request_received, send_response};
+use store_flows::global_get;
+
+#[no_mangle]
+pub fn run() {
+    request_received(|qry, _body| {
+        if let Some(challenge) = qry.get("challenge") {
+            if let Some(challenge) = challenge.as_str() {
+                if let Some(v) = global_get("web3bounty_email_challenge") {
+                    if let Some(ch) = v.get("challenge") {
+                        if challenge == ch.as_str().unwrap_or_default() {
+                            let account = v.get("account").unwrap();
+                            let email = v.get("email").unwrap();
+
+                            let record = serde_json::json!({
+                                "Web3Account": account,
+                                "Email": email
+                            });
+                            create_record(
+                                &std::env::var("AIRTABLE_ACCOUNT_NAME").unwrap(),
+                                &std::env::var("AIRTABLE_BASE_ID").unwrap(),
+                                "Email",
+                                record,
+                            );
+
+                            return send_response(
+                                200,
+                                vec![(
+                                    String::from("Content-Type"),
+                                    std::env::var("text/html").unwrap(),
+                                )],
+                                r#"Email has been verified.
+                                <script>
+                                    setTimeout(function() {
+                                        window.close();
+                                    }, 1500);
+                                </script>
+                                "#
+                                .as_bytes()
+                                .to_vec(),
+                            );
+                        }
+                    }
+                }
+            }
+        }
+        send_response(400, vec![], "Invalid challenge code".as_bytes().to_vec());
+    });
+}
